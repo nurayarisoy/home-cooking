0811011838f2db5072e2f96556c4970703d32621
nurayarisoy <cevriyeceven@gmail.com>
2024-02-26 18:04:25 +0100

    added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,18 @@
 import React from 'react';
 import { DataProvider } from '../context/DataContext';
 import '../styles/globals.css';
+import Navbar from '../componets/Navbar';
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <DataProvider>
+   {/* Navbar bileşenini DataProvider bileşeninin içine ekleyin */}
+        <Navbar/>
       <Component {...pageProps} />
     </DataProvider>
   );
 }
 
 export default MyApp;
+
