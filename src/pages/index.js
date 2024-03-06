5f0143670b00ff5311d179e6fc763a6bc28873b3
nurayarisoy <cevriyeceven@gmail.com>
2024-03-07 00:17:02 +0100

    navbar footer added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
 // pages/index.js
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer'; // Footer bileşenini import edin
 import 'tailwindcss/tailwind.css';
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
-      <h1>Home Page</h1>
-      <p>Welcome to my website! selam</p>
+      <div class="content">
+    <video controls>
+      <source src="/video.mp4" type="video/mp4" />
+      Tarayıcınız video etiketini desteklemiyor.
+    </video>
+  </div>
+ 
+      <Footer /> {/* Footer bileşenini ekleyin */}
     </div>
   );
 };
