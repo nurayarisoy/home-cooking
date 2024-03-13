d9e3a2e1e40689b043a9975e8346f9cbbf75433a
nurayarisoy <cevriyeceven@gmail.com>
2024-03-13 22:44:46 +0100

    login.css

--- src/pages/login/ChefLogin.js
+++ src/pages/login/ChefLogin.js
@@ -6,9 +6,16 @@ const ChefLoginPage = () => {
     <div>
       <Navbar /> {/* Navbar bileşenini ekleyin */}
       <h1>Chef Login</h1>
+      <form>
+        {/* Kullanıcı adı ve şifre alanları */}
+        {/* Resim yükleme alanı */}
+        {/* Tarif yazma alanı */}
+        <button type="submit">Login</button>
+      </form>
       {/* Diğer içeriği buraya ekleyin */}
     </div>
   );
 };
 
 export default ChefLoginPage;
+
