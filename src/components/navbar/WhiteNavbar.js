643ccbe851af7341fe7bbcee613644c4f74848d9
nurayarisoy <cevriyeceven@gmail.com>
2024-03-18 16:29:09 +0100

    navbar update

--- src/components/navbar/WhiteNavbar.js
+++ src/components/navbar/WhiteNavbar.js
@@ -1,20 +1,17 @@
 // WhiteNavbar.js
 
 import React from 'react';
+import 'tailwindcss/tailwind.css'
+
 
 const WhiteNavbar = () => {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-            <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
-            <div className="flex items-center space-x-6 rtl:space-x-reverse">
-                <a href="tel:5541251234" className="text-sm text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-                <a href="#" className="text-sm text-blue-600 dark:text-blue-500 hover:underline">Login</a>
-            </div>
-        </div>
+    <nav className="bg-gray-200 p-4 flex justify-between items-center">
+      <div className="text-lg font-bold">Logo</div>
+      <div className="space-x-4">
+        <a href="#" className="text-gray-700 hover:text-gray-900">Chef Login</a>
+        <a href="#" className="text-gray-700 hover:text-gray-900">Customer Login</a>
+      </div>
     </nav>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { DataProvider } from '../context/DataContext';
 import '../styles/globals.css'; // Tailwind CSS global stil dosyasını ekleyin
 import 'tailwindcss/tailwind.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import WhiteNavbar from '../components/navbar/WhiteNavbar';
 import Footer from'../components/footer/Footer';
 import Layout from'../components/layout/Layout';
