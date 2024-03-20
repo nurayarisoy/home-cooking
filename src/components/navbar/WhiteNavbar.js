5d7e769c493295683a14b64e2f0dff85d483b69f
nurayarisoy <cevriyeceven@gmail.com>
2024-03-20 22:43:19 +0100

    login added

--- src/components/navbar/WhiteNavbar.js
+++ src/components/navbar/WhiteNavbar.js
@@ -1,15 +1,21 @@
 // WhiteNavbar.js
 
 import React from 'react';
-import 'tailwindcss/tailwind.css'
-
+import { useRouter } from 'next/router';
+import 'tailwindcss/tailwind.css';
 
 const WhiteNavbar = () => {
+  const router = useRouter();
+
+  const handleChefLoginClick = () => {
+    router.push('/login');
+  };
+
   return (
     <nav className="bg-gray-200 p-4 flex justify-between items-center">
       <div className="text-lg font-bold">Logo</div>
       <div className="space-x-4">
-        <a href="#" className="text-gray-700 hover:text-gray-900">Chef Login</a>
+        <a href="#" className="text-gray-700 hover:text-gray-900" onClick={handleChefLoginClick}>Chef Login</a>
         <a href="#" className="text-gray-700 hover:text-gray-900">Customer Login</a>
       </div>
     </nav>
@@ -17,4 +23,5 @@ const WhiteNavbar = () => {
 };
 
 export default WhiteNavbar;
+;
 
