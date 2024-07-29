eb5a96019d8f95ef9f90e0f1e9c096b5b4344ce8
nurayarisoy <cevriyeceven@gmail.com>
2024-07-29 22:30:27 +0200

    added video

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,15 +1,17 @@
-// src/components/main/Main.js
 import '../../../styles/tailwind.css';
 
 const Main = ({ children }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <main className="animate-color-change flex flex-col items-center justify-center flex-grow w-full max-w-4xl px-4 py-8 bg-white shadow-md rounded-lg">
-        <img
-          src="/chef3.png"
-          alt="Centered Image"
-          className="mb-8 w-full h-auto max-w-sm object-cover rounded-lg"
+      <main className="relative flex flex-col items-center justify-center flex-grow w-full overflow-hidden">
+        
+        {/* Video bileşeni eklendi */}
+        <video
+          src="/video.mp4" // Videonuzun yolu
+          controls
+          className="w-full h-screen object-cover"
         />
+
         {children}
         <h1 className="text-3xl font-bold mb-4">Home Cooking</h1>
         <p className="text-gray-700 text-center">
